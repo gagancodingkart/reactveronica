2289ed0de766e192c453e8d87a307284f6b954ef
Gagan jain <59063393+gagancodingkart@users.noreply.github.com>
2019-12-27 13:53:39 +0530

    Add files via upload

--- src/Commoncomponent.js
+++ src/Commoncomponent.js
@@ -3,7 +3,9 @@ import React,{Component} from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './CommonComponent.css';
 import Home from "./components/Home.js";
+import Header from "./components/Header.js";
 import 'tachyons';
+import ProductCategories from "./components/ProductCategories.js";
 class Commoncomponent extends Component {
 // class App extends Component {
   render() {
@@ -12,6 +14,8 @@ class Commoncomponent extends Component {
         <Router>
           <div>
             <Route exact path="/" component={Home} />
+            <Route exact path="/Header" component={Header} />
+            <Route exact path="/ProductCategories" component={ProductCategories} />
           </div>
         </Router>
       </div>
