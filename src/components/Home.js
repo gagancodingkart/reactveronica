e4fd40eff6b383696f480fb94fc180617b9e61c0
Gagan jain <59063393+gagancodingkart@users.noreply.github.com>
2020-01-02 09:25:43 +0530

    Add files via upload

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,51 +1,114 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import { Col, Row, Container, Button, Spinner, Pagination } from 'react-bootstrap';
 import { WooCommerce } from "./WooConnection.js";
-import Navbar from 'react-bootstrap/Navbar';
-import { ButtonInput } from 'react-bootstrap';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import Header from "./Header.js";
-class Home extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      error: null,
-      isLoaded: false,
-      items: []
-    };
-  }
-
-   getData() {
-    const that = this;
-    WooCommerce.getAsync("products?per_page=100").then(function(result) {
-      console.log(result);
+import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom';
+import axios from 'axios';
+ class Product extends Component {
+       constructor(props) {
+          super(props);
+           this.pageChanged = this.pageChanged.bind(this);
+          this.state = {
+             error: null,
+             isLoaded: false,
+             currentPage: 1,
+             per_page_product: 10,
+             total_items:null,
+             category: [],
+             items: []
+            
+          }
+            
+        }
+     getData(page){
+     const that = this;
+      if (page) {
+        var page = page;
+     }else{
+      var page = 1;
+     }
+    WooCommerce.getAsync('products?per_page='+that.state.per_page_product+'&page='+page)
+     .then(function(result) {
+      console.log(JSON.parse(result.toJSON().body));
       that.setState({
-        isLoaded: true,
-        items: JSON.parse(result.toJSON().body)
-      });
-    });
+          isLoaded: true,
+          total_items: result.headers['x-wp-total'],
+          items: JSON.parse(result.toJSON().body),
+        })
+            
+      })
   }
-
-componentDidMount() {
+ 
+  
+  componentDidMount(){
     this.getData();
   }
+   pageChanged(e) {
+    this.setState({ currentPage: e.target.text });
+    this.getData(e.target.text);
+  }
+  productlist(){
+    return (
+        this.state.items.map((item) => {
+          return (
+                <div className="main_div_style bg-light-purple dib pa4 tc">
+              <Col key={item.id} xs={3} className="product_item"> <div key={item.id}>
+                <img width="100%" alt="product" src={item.images[0].src} />
+                <Link to={`/product/${item.id}`}><h4>{item.name}</h4></Link>
+                <h6> ${item.price}</h6>
+                 <Button variant="outline-primary" className="add_to_cart_btn" href={`/product/${item.id}`}>Buy Now</Button>
+                 {/* <Link to={`/product/${item.id}`}  className="add_to_cart_btn">Buy Now</Link> */}
+                  </div>
+              </Col>
+              </div>
+             );
+            })    
+    );
+  }
+  
+//render  list
+  render () {
+      console.log(this.state.currentPage);      
+      const all_page = this.state.total_items / this.state.per_page_product;
+      let active = this.state.currentPage;
+      let Pitems = [];   
+ 
+      for (let number = 1; number <= all_page; number++) {
+        Pitems.push(
+          <Pagination.Item key={number} active={number === active}>
+            {number}
+          </Pagination.Item>
+          
+          ,
+        );
+      }
 
- render() {
-     return (
-      <div>
+const paginationBasic = (
+  <div>
+    <Pagination onClick={this.pageChanged}>{Pitems}</Pagination>
+   
+  </div>
+);
+                  return(
+        <Container>
+       
+          <Row>
 
-      <div className="main_div_style bg-light-purple dib pa4 tc">
-             <Header />
-        {this.state.items.map((val, index) => (
-        <div className="col-sm-4" key={index+1}>            
-          <img src={val.images[0].src} alt="" />
-          <h2>${val.price}</h2>
-          <p>{val.name}</p>
-        </div>
-       ))}
-      </div>
-      </div>
-      )
-  }    
+<Col xs={9}>
+   
+         <Row>
+        {this.productlist()}
+        </Row>
+</Col>
+</Row>        
+        
+           <div className="page_cont">
+           {paginationBasic}
+          </div>  
+            
+          </Container>
+    )
+               
+        }
+    }
 
-}
-export default Home;
+export default Product;
